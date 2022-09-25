a379a68246909c10bad3811784343c8883ed0428
Kelsey Mwongeli <kelseykm.git@gmail.com>
2022-09-25 18:10:37 +0300

    revert: print exception message

--- ketter/__main__.py
+++ ketter/__main__.py
@@ -148,7 +148,7 @@ async def main():
         for idx, result in enumerate(results):
             if isinstance(result, Exception):
                 print(
-                    f"{error_banner()} {format_user_submitted(urls[idx])}: {repr(result)}")
+                    f"{error_banner()} {format_user_submitted(urls[idx])}: {result}")
 
 
 if __name__ == "__main__":
