7ce994dc70242a80c5199431eb4744f9c362aca9
Kelsey Mwongeli <kelseykm.git@gmail.com>
2022-09-26 01:10:00 +0300

    feat: add doc strings to errors

--- ketter/errors.py
+++ ketter/errors.py
@@ -2,12 +2,18 @@
 
 
 class KetterHTTPError(Exception):
+    """for errors encountered while downloading"""
+
     ...
 
 
 class KetterHTTPHeaderError(Exception):
+    """for errors relating to custom headers"""
+
     ...
 
 
 class KetterInvalidFileError(Exception):
+    """for errors relating to url file"""
+
     ...
